96b851336c922904365d3face1554fb1460dc46c
murd0c96 <matiasnoriega86@gmail.com>
2015-10-02 12:51:39 -0300

    Create ItemCarrito.java

---
+++ ItemCarrito.java
@@ -0,0 +1,40 @@
+package ar.edu.uno.progconobjetos1.carritodecompras.modulo;
+
+public class ItemCarrito {
+	private Producto producto;
+	private Integer cantidad;
+	private Double costoItem;
+	
+	//Constructor de la clase
+	public ItemCarrito(Producto producto, Integer cantidad){
+		this.setCantidad(cantidad);
+		this.setProducto(producto);
+		this.setCostoItem(cantidad*(producto.getPrecio()));
+	}
+	
+
+
+	//Getters y setters
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	public Double getCostoItem() {
+		return costoItem;
+	}
+	public void setCostoItem(Double costoItem) {
+		this.costoItem = costoItem;
+	}
+	
+	
+	
+	
+}

ddf0646a897ac56a1bbd65f3df76c5fef56d4619
murd0c96 <matiasnoriega86@gmail.com>
2015-10-06 23:26:43 -0300

    Update ClientePreferencial.java

--- ClientePreferencial.java
+++ ClientePreferencial.java
@@ -17,10 +17,14 @@ public class ClientePreferencial extends Cliente{
 	}
 	
 	public void comprar(Carrito carrito){
-		
-		if(this.getTarjeta().verificarTarjeta(carrito.getCostoCarrito())==true)
-			System.out.println("Felicidades "+this.getNombre()+", la compra de su carrito "+carrito.getNombreCarrito()+" ha sido realizada con exito!\nSe han debitado $"+carrito.getCostoCarrito()+" de su tarjeta.\n\nLos productos seran enviados a "+this.getDireccionEnvio()+"\n-------------------------------------------------------");
-		
+		if(carrito.getEstadoVendido()==false){
+			if(this.getTarjeta().verificarTarjeta(carrito.getCostoCarrito()*getPorcentajeDescuento())==true){
+				System.out.println("Felicidades "+this.getNombre()+", la compra de su carrito "+carrito.getNombreCarrito()+" ha sido realizada con exito!\nSu descuento ha sido del "+(100-(this.getPorcentajeDescuento()*100))+"% y se han debitado $"+carrito.getCostoCarrito()*getPorcentajeDescuento()+" de su tarjeta.\n\nLos productos seran enviados a "+this.getDireccionEnvio()+"\n-------------------------------------------------------");
+				carrito.setEstadoVendido(true);
+			}
+		}
+		else
+			System.out.println("El carrito ya fue comprado!");
 	}
 	
 }
